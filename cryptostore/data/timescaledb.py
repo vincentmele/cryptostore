0829b699626eff73d1483049de7857837c303c7e
Vincent Mele <vincentmele@gmail.com>
2020-04-11 22:16:09 +0100

    reformat which touched a lot of files; added iterating index to book and trades to ensure uniqueness of items (to avoid duplicates with multiple streams feeding data in); added constraints and primary keys to tables

--- cryptostore/data/timescaledb.py
+++ cryptostore/data/timescaledb.py
@@ -13,7 +13,7 @@ from datetime import timezone, datetime as dt
 from cryptofeed.defines import TRADES, L2_BOOK, L3_BOOK, TICKER, FUNDING, OPEN_INTEREST
 
 from cryptostore.data.store import Store
-#from cryptostore.engines import StorageEngines
+# from cryptostore.engines import StorageEngines
 
 import psycopg2
 from psycopg2.extras import execute_values
@@ -24,18 +24,18 @@ LOG = logging.getLogger('cryptostore')
 
 
 def chunk(iterable, length):
-    return (iterable[i : i + length] for i in range(0, len(iterable), length))
+    return (iterable[i: i + length] for i in range(0, len(iterable), length))
 
 
 class TimescaleDB(Store):
-    def __init__(self, config: dict, connection = None):
+    def __init__(self, config: dict, connection=None):
         self.data = None
         self.host = config.get('host')
         self.user = config.get('user')
         self.pw = config.get('pw')
         self.db = config.get('db')
         self.page_size = config.get('page_size', 5000)
-        self.chunk_interval = config.get('chunk_interval', 12) # in hours
+        self.chunk_interval = config.get('chunk_interval', 12)  # in hours
         self.table = None
         self.table_exists = False
         self.conn = None  # StorageEngines.timescaledb.TimeseriesDB(connection)
@@ -63,9 +63,16 @@ class TimescaleDB(Store):
         if not self.data:
             return
 
+        used_ts = {}
         for entry in self.data:
-            entry['timestamp'] = dt.fromtimestamp(entry['timestamp'], timezone.utc )
-            entry['receipt_timestamp'] = dt.fromtimestamp(entry['receipt_timestamp'], timezone.utc )
+
+            if data_type == L2_BOOK or data_type == L3_BOOK or data_type == TRADES:
+                idx = used_ts.get(entry['timestamp'], 0)
+                entry['idx'] = None if idx == 0 else idx
+                used_ts[entry['timestamp']] = idx + 1
+
+            entry['timestamp'] = dt.fromtimestamp(entry['timestamp'], timezone.utc)
+            entry['receipt_timestamp'] = dt.fromtimestamp(entry['receipt_timestamp'], timezone.utc)
             entry['feed'] = entry.get('feed', exchange)
             entry['id'] = entry.get('id')
             entry['pair'] = pair
@@ -76,7 +83,7 @@ class TimescaleDB(Store):
         self._create_table_if_not_exists(data_type, table)
         self._insert_data(table, data_type)
 
-    def _set_table(self, exchange, data_type, pair = None):
+    def _set_table(self, exchange, data_type, pair=None):
         """Utility function to normalise table name(s)"""
         self.table = f'{data_type}_{exchange}'.lower()
         return self.table
@@ -101,11 +108,14 @@ class TimescaleDB(Store):
                     f'timestamp TIMESTAMPTZ NOT NULL, ' \
                     f'feed VARCHAR, ' \
                     f'pair VARCHAR, ' \
+                    f'idx INTEGER, ' \
                     f'side VARCHAR, ' \
                     f'id VARCHAR NULL,' \
                     f'amount NUMERIC, ' \
                     f'price NUMERIC, ' \
-                    f'receipt_timestamp TIMESTAMPTZ ' \
+                    f'receipt_timestamp TIMESTAMPTZ, ' \
+                    f'PRIMARY KEY (timestamp, feed, pair), ' \
+                    f'CONSTRAINT unique_trades_const UNIQUE (timestamp, feed, pair, idx)' \
                     f');'
         elif data_type == TICKER:
             query = f'CREATE TABLE IF NOT EXISTS {table} (' \
@@ -115,19 +125,22 @@ class TimescaleDB(Store):
                     f'id VARCHAR NULL, ' \
                     f'bid NUMERIC, ' \
                     f'ask NUMERIC, ' \
-                    f'receipt_timestamp TIMESTAMPTZ ' \
+                    f'receipt_timestamp TIMESTAMPTZ, ' \
+                    f'PRIMARY KEY (timestamp, feed, pair)' \
                     f');'
         elif data_type == L3_BOOK or data_type == L2_BOOK:
             query = f'CREATE TABLE IF NOT EXISTS {table} (' \
                     f'timestamp TIMESTAMPTZ NOT NULL, ' \
                     f'feed VARCHAR, ' \
                     f'pair VARCHAR, ' \
+                    f'idx INTEGER, ' \
                     f'delta BOOLEAN, ' \
                     f'side VARCHAR, ' \
                     f'id VARCHAR NULL, ' \
                     f'size NUMERIC, ' \
                     f'price NUMERIC, ' \
-                    f'receipt_timestamp TIMESTAMPTZ ' \
+                    f'receipt_timestamp TIMESTAMPTZ, ' \
+                    f'CONSTRAINT unique_{data_type}_const UNIQUE (timestamp, feed, pair, idx)' \
                     f');'
         elif data_type == FUNDING:
             return NotImplemented  # TODO: Seems to need to store arbitrary key/value pairs, see influx.py?
@@ -137,7 +150,8 @@ class TimescaleDB(Store):
                     f'feed VARCHAR, ' \
                     f'pair VARCHAR, ' \
                     f'open_interest NUMERIC, ' \
-                    f'receipt_timestamp TIMESTAMPTZ ' \
+                    f'receipt_timestamp TIMESTAMPTZ, ' \
+                    f'PRIMARY KEY (timestamp, feed, pair)' \
                     f');'
 
         with self.conn.cursor() as cur:
@@ -146,16 +160,24 @@ class TimescaleDB(Store):
                 self.conn.commit()
                 cur.execute(f'CREATE INDEX ON {table} (feed, pair);')
                 cur.execute(f'SELECT create_hypertable(%s, %s);', (table, 'timestamp',))
-                if data_type == L2_BOOK or data_type == L3_BOOK:
-                    cur.execute(f"SELECT set_chunk_time_interval(%s, %s * interval '1 hours');", (table, self.chunk_interval ))
+                if data_type == L2_BOOK or data_type == L3_BOOK or data_type == TRADES:
+                    cur.execute(f"SELECT set_chunk_time_interval(%s, %s * interval '1 hours');",
+                                (table, self.chunk_interval))
+                    compress_sql = f'ALTER TABLE {table} SET (' \
+                                   f' timescaledb.compress, ' \
+                                   f'timescaledb.compress_segmentby = "feed, pair, idx"' \
+                                   f');'
                 else:
-                    cur.execute(f"SELECT set_chunk_time_interval(%s, %s * interval '1 hours');", (table, self.chunk_interval * 2))
+                    cur.execute(f"SELECT set_chunk_time_interval(%s, %s * interval '1 hours');",
+                                (table, self.chunk_interval * 2))
+                    compress_sql = f'ALTER TABLE {table} SET (' \
+                                   f' timescaledb.compress, ' \
+                                   f'timescaledb.compress_segmentby = "feed, pair"' \
+                                   f');'
 
-                cur.execute(f'ALTER TABLE {table} SET ('
-                            f' timescaledb.compress, '
-                            f'timescaledb.compress_segmentby = "feed, pair"'
-                            f');')
-                cur.execute(f"SELECT add_compress_chunks_policy(%s, %s * interval '1 hour');", (table, self.chunk_interval * 3))
+                cur.execute(compress_sql)
+                cur.execute(f"SELECT add_compress_chunks_policy(%s, %s * interval '1 hour');",
+                            (table, self.chunk_interval * 3))
 
                 self.conn.commit()
             except Exception as e:
@@ -173,13 +195,13 @@ class TimescaleDB(Store):
         template = ''
         if data_type == TRADES:
             if id in self.data:
-                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, side, id, amount, price, receipt_timestamp) VALUES  %s " \
+                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, idx, side, id, amount, price, receipt_timestamp) VALUES  %s " \
                              f"ON CONFLICT DO NOTHING"
-                template = '(%(timestamp)s, %(feed)s, %(pair)s, %(side)s, %(id)s, %(amount)s, %(price)s, %(receipt_timestamp)s)'
+                template = '(%(timestamp)s, %(feed)s, %(pair)s, %(idx), %(side)s, %(id)s, %(amount)s, %(price)s, %(receipt_timestamp)s)'
             else:
-                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, side, amount, price, receipt_timestamp) VALUES  %s " \
+                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, idx, side, amount, price, receipt_timestamp) VALUES  %s " \
                              f"ON CONFLICT DO NOTHING"
-                template = '(%(timestamp)s, %(feed)s, %(pair)s, %(side)s, %(amount)s, %(price)s, %(receipt_timestamp)s)'
+                template = '(%(timestamp)s, %(feed)s, %(pair)s, %(idx)s, %(side)s, %(amount)s, %(price)s, %(receipt_timestamp)s)'
 
         elif data_type == TICKER:
             if id in self.data:
@@ -192,13 +214,13 @@ class TimescaleDB(Store):
                 template = '(%(timestamp)s, %(feed)s, %(pair)s, %(bid)s, %(ask)s, %(receipt_timestamp)s)'
         elif data_type == L3_BOOK or data_type == L2_BOOK:
             if id in self.data:
-                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, delta, side, id, size, price, receipt_timestamp) VALUES  %s " \
+                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, idx, delta, side, id, size, price, receipt_timestamp) VALUES  %s " \
                              f"ON CONFLICT DO NOTHING"
-                template = '(%(timestamp)s, %(feed)s, %(pair)s, %(delta)s, %(side)s, %(id)s, %(size)s, %(price)s, %(receipt_timestamp)s)'
+                template = '(%(timestamp)s, %(feed)s, %(pair)s, %(idx)s, %(delta)s, %(side)s, %(id)s, %(size)s, %(price)s, %(receipt_timestamp)s)'
             else:
-                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, delta, side, size, price, receipt_timestamp) VALUES  %s " \
+                insert_sql = f"INSERT INTO {table} (timestamp, feed, pair, idx, delta, side, size, price, receipt_timestamp) VALUES  %s " \
                              f"ON CONFLICT DO NOTHING"
-                template = '(%(timestamp)s,  %(feed)s, %(pair)s, %(delta)s, %(side)s, %(size)s, %(price)s, %(receipt_timestamp)s)'
+                template = '(%(timestamp)s,  %(feed)s, %(pair)s, %(idx)s, %(delta)s, %(side)s, %(size)s, %(price)s, %(receipt_timestamp)s)'
 
         elif data_type == FUNDING:
             return NotImplemented
